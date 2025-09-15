dc3aa2785073f4bea512448b5d5ec214533c9b15
Hovohoangduy <h.hoangduy2002@gmail.com>
2025-09-15 09:33:59 +0700

    updates vit

--- src/model.py
+++ src/model.py
@@ -1,29 +1,20 @@
 import torch
 from torchvision import models
 import torch.nn as nn
+from transformers import ViTForImageClassification
 
 class Classifier(nn.Module):
     def __init__(self, num_classes=2, pretrained=True, freeze_backbone=True):
-        super(Classifier, self).__init__()
-        weights = models.ResNet152_Weights.IMAGENET1K_V2 if pretrained else None
-        self.model = models.resnet152(weights=weights)
-        if freeze_backbone:
-            for param in self.model.parameters():
-                param.requires_grad = False
-
-        in_features = self.model.fc.in_features
-        self.model.fc = nn.Sequential(
-            nn.Linear(in_features, 512),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(512, 128),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(128, num_classes)
+        super().__init__()
+        model_name = "google/vit-base-patch16-224-in21k"
+        self.model = ViTForImageClassification.from_pretrained(
+            model_name, num_labels=num_classes
         )
 
-        for param in self.model.fc.parameters():
-            param.requires_grad = True
+        if freeze_backbone:
+            for name, param in self.model.named_parameters():
+                if "classifier" not in name:  # chỉ fine-tune head
+                    param.requires_grad = False
 
     def forward(self, x):
-        return self.model(x)
+        return self.model(x).logits
