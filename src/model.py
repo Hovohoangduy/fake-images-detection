ca75b6cbd1b3eaa0024a79cb6a30ef2d8b820438
Hovohoangduy <h.hoangduy2002@gmail.com>
2025-09-16 14:18:39 +0700

    update swin transformer

--- src/model.py
+++ src/model.py
@@ -1,15 +1,22 @@
 import torch
 from torchvision import models
 import torch.nn as nn
-from transformers import ViTForImageClassification
+from transformers import ViTForImageClassification, SwinForImageClassification
 
 class Classifier(nn.Module):
     def __init__(self, num_classes=2, pretrained=True, freeze_backbone=True):
         super().__init__()
-        model_name = "google/vit-base-patch16-224-in21k"
-        self.model = ViTForImageClassification.from_pretrained(
-            model_name, num_labels=num_classes
-        )
+        model_name = "microsoft/swin-base-patch4-window7-224"
+        if pretrained:
+            self.model = SwinForImageClassification.from_pretrained(
+                model_name,
+                num_labels=num_classes,
+                ignore_mismatched_sizes=True
+            )
+        else:
+            config = SwinForImageClassification.config_class.from_pretrained(model_name)
+            config.num_labels = num_classes
+            self.model = SwinForImageClassification(config)
 
         if freeze_backbone:
             for name, param in self.model.named_parameters():
